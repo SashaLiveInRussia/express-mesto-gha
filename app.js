a83da209db219dfb79ae19ff29664d62883db5c4
Aleksandra <nickshi3477@gmail.com>
2022-08-19 02:47:27 +0300

    1

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use((err, req, res, next) => {
 
   const statusCode = err.statusCode || 500;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
-
   res.status(statusCode).send({ message });
   return next();
 });
