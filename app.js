d41759f413f49371c825dad7193f5525f9149a67
Aleksandra <nickshi3477@gmail.com>
2022-08-20 16:28:35 +0300

    r

--- app.js
+++ app.js
@@ -8,6 +8,37 @@ const NotFoundError = require('./errors/NotFoundError');
 const app = express();
 const { PORT = 3000 } = process.env;
 
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000'
+];
+
+app.use(function (req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // проверяем, что источник запроса есть среди разрешённых 
+
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(router);
 app.use(errors());
@@ -32,4 +63,4 @@ app.use((err, req, res, next) => {
   return next();
 });
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => { });
