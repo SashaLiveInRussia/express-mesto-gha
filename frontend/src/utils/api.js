f2311d4487938b6668f036d09c54b253300cdd9e
Aleksandra <nickshi3477@gmail.com>
2022-08-20 17:23:39 +0300

    f

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -84,9 +84,9 @@ export class Api {
 
 
 const api = new Api({
-	baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-39',
+	baseUrl: 'https://api.sasha.nomoredomains.sbs',
 	headers: {
-		authorization: 'adaf3729-939a-4351-9992-562ebfc14bb0',
+		// authorization: 'adaf3729-939a-4351-9992-562ebfc14bb0',
 		'Content-Type': 'application/json'
 	}
 });
